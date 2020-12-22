e791098b6206c3029727a52f0e2070b38fa810cf
AnTEQ1 <an.andrey.90@gmail.com>
2020-12-22 23:55:51 +0200

    Изменено создание объекта

--- src/com/academy/telesens/lesson8/ht/lambda/task1/HtLambda1.java
+++ src/com/academy/telesens/lesson8/ht/lambda/task1/HtLambda1.java
@@ -2,9 +2,7 @@ package com.academy.telesens.lesson8.ht.lambda.task1;
 
 public class HtLambda1 {
     public static void main(String[] args) {
-        BoolInterfaсe oper = (x) -> {
-            return x >= 10 && x <= 20;
-        };
+        BoolInterfaсe oper = (x) -> x >= 10 && x <= 20;
         System.out.println(oper.limit(15));
         System.out.println(oper.limit(21));
         System.out.println(oper.limit(9));
