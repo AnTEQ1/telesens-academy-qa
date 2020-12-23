8b8fa1f0e943bb5bfcc4fd49889754e2f3fb2639
AnTEQ1 <an.andrey.90@gmail.com>
2020-12-23 23:22:17 +0200

    Удаление пробелов

---
+++ src/com/academy/telesens/lesson8/ht/lambda/task4/DeleteSpaces.java
@@ -0,0 +1,5 @@
+package com.academy.telesens.lesson8.ht.lambda.task4;
+
+public interface DeleteSpaces {
+    String spaceDeletion (String s);
+}
---
+++ src/com/academy/telesens/lesson8/ht/lambda/task4/HtLambda3.java
@@ -0,0 +1,11 @@
+package com.academy.telesens.lesson8.ht.lambda.task4;
+/*4) Создайте лямбда-выражение, которое удаляет все пробелы из заданной строки и возвращает результат.
+*	Продемонстрируйте работу лямбда-выражения.
+*	"Helen Abdel Leon abc array var jMeter Selenium Yaguar" => "HelenAbdelLeonabcarrayvarjMeterSeleniumYaguar"
+*/
+public class HtLambda3 {
+    public static void main(String[] args) {
+        DeleteSpaces deleteSpaces = s -> s.replace(" ", "");
+        System.out.println(deleteSpaces.spaceDeletion("Helen Abdel Leon abc array var jMeter Selenium Yaguar"));
+    }
+}
