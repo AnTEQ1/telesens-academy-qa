421c1d3bfa0c9f57e99d240cffc799b37819b4fa
AnTEQ1 <an.andrey.90@gmail.com>
2020-12-23 23:26:22 +0200

    Переименован класс

--- src/com/academy/telesens/lesson8/ht/lambda/task4/HtLambda4.java
+++ src/com/academy/telesens/lesson8/ht/lambda/task4/HtLambda4.java
@@ -3,7 +3,7 @@ package com.academy.telesens.lesson8.ht.lambda.task4;
 *	Продемонстрируйте работу лямбда-выражения.
 *	"Helen Abdel Leon abc array var jMeter Selenium Yaguar" => "HelenAbdelLeonabcarrayvarjMeterSeleniumYaguar"
 */
-public class HtLambda3 {
+public class HtLambda4 {
     public static void main(String[] args) {
         DeleteSpaces deleteSpaces = s -> s.replace(" ", "");
         System.out.println(deleteSpaces.spaceDeletion("Helen Abdel Leon abc array var jMeter Selenium Yaguar"));
